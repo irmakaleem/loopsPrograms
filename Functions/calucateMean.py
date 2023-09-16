e343cb0a954123777bbe7c5e1ee9252ea07cec8a
irma <irmakaleem12@gmail.com>
2023-09-16 10:41:02 +0500

    loop/break&continue/matchcase

---
+++ Functions/calucateMean.py
@@ -0,0 +1,16 @@
+def calculatedGmean(a,b):
+    mean= a*b/a+b
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")    
+a=7
+b=12
+isGreater(a,b)
+c=2
+d=1
+isGreater(c,d)
+calculatedGmean(a,b)
+calculatedGmean(c,d)
